297fb27766c73e8a1d6c3349ef067d42fdecc7b9
bronzehat <zelasayy+otru@gmail.com>
2019-03-22 12:12:29 -0700

    add more description in docstrings of tests

---
+++ first_simple_pytests/tests/test_lists_ops.py
@@ -0,0 +1,17 @@
+import lists_ops
+import pytest
+
+# adds numbers in range of the given number to the list
+def test_lists_append():
+    append_resuls = lists_ops.lists_append(8)
+    assert append_resuls == [0, 1, 2, 3, 4, 5, 6, 7]
+
+# returns the length of the given list
+def test_lists_length():
+    length_result = lists_ops.lists_length(["element"])
+    assert length_result == 1
+
+# reverses the given list
+def test_lists_reverse(l):
+    reverse_result = lists_ops.lists_reverse([0, 1, 2])
+    assert reverse_result == [2, 1, 0]
