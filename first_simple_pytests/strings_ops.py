185816a7cebafb1342a7ac4a8e1185830e3469ee
bronzehat <zelasayy+otru@gmail.com>
2019-03-21 22:55:18 -0700

    "adding first simple tests for strings"

---
+++ first_simple_pytests/strings_ops.py
@@ -0,0 +1,11 @@
+def strings_concat(a, b):
+    return a + b
+
+def strings_common_symbols(a, b):
+    if x in a:
+        if x in b:
+            return(x)
+
+def strings_first_symbol(a, b):
+    if a[0] == b[0]:
+        return (a[0])
---
+++ first_simple_pytests/test_strings_ops.py
@@ -0,0 +1,13 @@
+import strings_ops
+
+def test_strings_concat():
+    concat_result = strings_ops.strings_concat("su", "per")
+    assert concat_result == "super"
+
+def test_strings_common_symbols():
+    common_result = strings_ops.strings_common_symbols("hi", "hello")
+    assert common_result == "h"
+
+def test_strings_first_symbol():
+    first_result = strings_ops.strings_first_symbol("otus", "october")
+    assert first_result == "o"
