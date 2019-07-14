7f1ac12d4d189e78590c41f49bfaac74ba6d1873
ASay <zelasayy@gmail.com>
2019-07-14 23:27:34 +0500

    added whl: screens and results of creating whl from the project

---
+++ whl/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="otus_auto_qa_asay",
+    version="0.2",
+    url="https://github.com/bronzehat/initial_tests",
+    author="ASayapova",
+    author_email="...",
+    description="Tests for my first tests in Otus course",
+    packages=find_packages(),
+    zip_safe=False,
+    requires=[],
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: Ubuntu"]
+)
